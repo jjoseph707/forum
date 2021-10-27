943b75091c3454dba39663a1200414b4c463b064
jason <jjoseph301@protonmail.coM>
2021-10-27 02:20:20 -0700

    added board+post views

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    #boards
+    path('board/<str:pk>/',views.board,name='board'),
+    path('board/<str:pk>/<str:pk2>/',views.post,name='post')
 ]
--- base/views.py
+++ base/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from .models import Board, Post
 
-boards = [
-    {'id':1,'topic':'random'},
-    {'id':2,'topic':'finance'},
-    {'id':3,'topic':'anime'},
-]
 
 def home(request):
+    boards = Board.objects.all()
     context = {'boards':boards}
     return render(request,'base/home.html',context)
+
+def board(request, pk):
+    target_board = Board.objects.get(topic=pk)
+    posts = Post.objects.filter(board__topic=pk) #
+
+    context = {'board':target_board,'posts':posts}
+    return render(request,'base/board.html',context)
+
+def post(request, pk, pk2):
+    post = Post.objects.get(id=pk2)
+    context={'post':post}
+    return render(request,'base/post.html',context)
